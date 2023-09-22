b796f720d54f9268f7435b23318ddc233b433d23
yao.xie <1595341200@qq.com>
2023-09-22 10:46:35 +0800

    图

---
+++ src/graph/src/main.cpp
@@ -0,0 +1,50 @@
+/*
+ * @Author: yao.xie 1595341200@qq.com
+ * @Date: 2023-09-22 10:19:16
+ * @LastEditors: yao.xie 1595341200@qq.com
+ * @LastEditTime: 2023-09-22 10:40:52
+ * @FilePath: /cplusplus/src/graph/src/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置:
+ * https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <Utils.h>
+#include <dbg.h>
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+#include <gtest/gtest.h>
+using namespace std;
+TEST(graph, 1) {
+  vector<vector<int>> graph(4, vector<int>(4));
+  graph[0][1] = 1;
+  graph[1][0] = 1;
+  graph[1][2] = 1;
+  graph[2][1] = 1;
+  graph[2][3] = 1;
+  graph[3][2] = 1;
+  print2DArray(graph);
+}
+struct Node {
+  Node(int i, int j) : id(i), val(j) {}
+  int id{};
+  int val{};
+};
+ostream& operator<<(ostream& os, const Node& node) {
+  os << "node (" << node.id << ") : " << node.val;
+  return os;
+}
+TEST(graph, 2) {
+  vector<list<Node>> graph(4);
+  graph[0].emplace_back(1, 1);
+  graph[0].emplace_back(3, 1);
+  graph[1].emplace_back(2, 1);
+  graph[2].emplace_back(3, 1);
+  dbg(graph);
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
--- utils/include/Utils.h
+++ utils/include/Utils.h
@@ -2,7 +2,7 @@
  * @Author: yao.xie 1595341200@qq.com
  * @Date: 2023-09-12 18:25:37
  * @LastEditors: yao.xie 1595341200@qq.com
- * @LastEditTime: 2023-09-15 09:43:49
+ * @LastEditTime: 2023-09-22 10:27:21
  * @FilePath: /utils/include/Utils.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -10,23 +10,40 @@
 #ifndef UTILS_H
 #define UTILS_H
 #include <Log.h>
-#include <boost/type_index.hpp>
+
+#include <iostream>
 #include <thread>
+
+#include <boost/type_index.hpp>
 void bindThreadToCore(int coreId, std::thread::native_handle_type handle);
 void bindProcessToCore(int coreId);
-template <typename T> void lookType(const T &param) {
+template <typename T>
+void lookType(const T &param) {
   using boost::typeindex::type_id_with_cvr;
   SLOG(INFO) << "T=" << type_id_with_cvr<T>().pretty_name();
   SLOG(INFO) << "param=" << type_id_with_cvr<decltype(param)>().pretty_name();
 }
 
-template <typename T> void lookType() {
+template <typename T>
+void lookType() {
   using boost::typeindex::type_id_with_cvr;
   SLOG(INFO) << "T=" << type_id_with_cvr<T>().pretty_name();
 }
 
-template <typename E> constexpr auto toUType(E e) noexcept {
+template <typename E>
+constexpr auto toUType(E e) noexcept {
   return static_cast<std::underlying_type_t<E>>(e);
+}
+
+//打印二维数组
+template <typename T>
+void print2DArray(T &array) {
+  for (auto &i : array) {
+    for (auto &j : i) {
+      std::cout << j << " ";
+    }
+    std::cout << std::endl;
+  }
 }
 
 #endif /* UTILS_H */
