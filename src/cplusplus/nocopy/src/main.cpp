edcdeee36214e8acdeacd79751c145e3f6a9da5a
yao.xie <1595341200@qq.com>
2024-01-04 15:12:18 +0800

    nocopy

--- src/cplusplus/nocopy/src/main.cpp
+++ src/cplusplus/nocopy/src/main.cpp
@@ -2,17 +2,18 @@
  * @Author: yao.xie 1595341200@qq.com
  * @Date: 2024-01-04 14:20:47
  * @LastEditors: yao.xie 1595341200@qq.com
- * @LastEditTime: 2024-01-04 14:50:32
+ * @LastEditTime: 2024-01-04 14:55:13
  * @FilePath: /cplusplus/src/cplusplus/nocopy/src/main.cpp
- * @Description: 
- * 
- * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+ * @Description:
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
  */
 #include <unistd.h>
 
 #include <iostream>
 #include <memory>
 #include <vector>
+
 #include "XHD_MessageSenderReceiver.h"
 #define TOTALREAD 2
 std::string DefaultTopic = "123";
@@ -32,16 +33,16 @@ void TestWrite(int num) {
     auto test = std::make_shared<XHD_MessageSenderReceiver>(topic);
     if (test->Init()) {
       Sender.emplace_back(test);
-      SLOG(INFO)<< topic << " added" << std::endl;
+      SLOG(INFO) << topic << " added" << std::endl;
     }
     topic.clear();
   }
   uint64_t dataindex = 0;
   while (1) {
-    SLOG(INFO) << "index : " <<  index;
+    SLOG(INFO) << "index : " << index;
     auto ptr = TestBuffer.GetDataWritePtr(index);
     if (ptr != nullptr) {
-      SLOG(INFO)<< "data sending" << std::endl;
+      SLOG(INFO) << "data sending" << std::endl;
       ptr->array[0] = dataindex++;
       TestBuffer.ReleaseWrite(index, TOTALREAD);
       XHD_Message msg;
@@ -61,19 +62,19 @@ void TestRead(int index) {
   std::string topic = DefaultTopic + (ch);
   auto test = std::make_shared<XHD_MessageSenderReceiver>(topic);
   if (test->Init()) {
-    SLOG(INFO)<< topic << " connected" << std::endl;
+    SLOG(INFO) << topic << " connected" << std::endl;
   }
   XHD_Message msg;
   while (1) {
     if (true == test->RecvMsg(msg)) {
-      SLOG(INFO)<< "msg data:" << msg.mtext << std::endl;
+      SLOG(INFO) << "msg data:" << msg.mtext << std::endl;
       auto data = TestBuffer.GetDataReadPtr((int)msg.mtext);
       if (data != nullptr) {
-        SLOG(INFO)<< "data:" << data->array[0] << std::endl;
+        SLOG(INFO) << "data:" << data->array[0] << std::endl;
       }
       TestBuffer.ReleaseRead((int)msg.mtext);
     } else {
-      SLOG(INFO)<< "Can not get data" << std::endl;
+      SLOG(INFO) << "Can not get data" << std::endl;
     }
   }
 }
